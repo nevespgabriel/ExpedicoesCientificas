11f24502a75a0aa9ae3c9d656b85acde98c7824a
Gabriel Pereira Neves <157249929+nevespgabriel@users.noreply.github.com>
2025-01-31 22:56:25 -0300

    Update conn.js

--- src/config/conn.js
+++ src/config/conn.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 (async () => {
     try{
-        mongoose.connect(process.env.URI + process.env.DB);
+        await mongoose.connect(process.env.URI + process.env.DB);
         console.log("Conectado com sucesso.");
     } catch(error){
         console.log(error);
